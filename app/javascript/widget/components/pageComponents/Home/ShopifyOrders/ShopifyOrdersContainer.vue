7d8aad79c62ea861759ecb673316620a0517cf07
avnotaklu <sukhmander111101@gmail.com>
2025-07-17 11:37:25 +0530

    Track another order

--- app/javascript/widget/components/pageComponents/Home/ShopifyOrders/ShopifyOrdersContainer.vue
+++ app/javascript/widget/components/pageComponents/Home/ShopifyOrders/ShopifyOrdersContainer.vue
@@ -1,4 +1,5 @@
 <script setup>
+import SharedButton from 'shared/components/Button.vue';
 import { computed, onMounted } from 'vue';
 import { useStore } from 'dashboard/composables/store';
 import CustomerIdentificationBlock from './CustomerIdentificationBlock.vue';
@@ -22,6 +23,10 @@ const order = computed(() => store.getters['orders/getOrder']);
 const orderUiFlags = computed(() => store.getters['orders/getUiFlags']);
 
 const contact = computed(() => store.getters['contacts/getCurrentUser']);
+
+const resetOrder = () => {
+  store.dispatch('orders/resetOrder');
+};
 </script>
 
 <template>
@@ -40,5 +45,14 @@ const contact = computed(() => store.getters['contacts/getCurrentUser']);
     >
       {{ $t('ORDER_NOT_FOUND') }}
     </span>
+    <SharedButton
+      v-if="order"
+      class="flex h-[40px] min-w-full justify-center items-center"
+      @click="resetOrder"
+    >
+      <span class="items-center text-white text-base">
+        {{ $t('TRACK_ANOTHER_ORDER') }}
+      </span>
+    </SharedButton>
   </div>
 </template>
