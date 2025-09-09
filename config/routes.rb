9574b6e8be40e6a95d93cae60d3dcdffc7c95099
avnotaklu <sukhmander111101@gmail.com>
2025-09-09 17:43:53 +0530

    Manual template sync

--- config/routes.rb
+++ config/routes.rb
@@ -198,6 +198,7 @@ Rails.application.routes.draw do
             get :agent_bot, on: :member
             post :set_agent_bot, on: :member
             delete :avatar, on: :member
+            post :sync_templates, on: :member
           end
           resources :inbox_members, only: [:create, :show], param: :inbox_id do
             collection do
